862412cc7c0b24a7e0fe77eaa0de68c289f3aad5
Marko Kupresanin <kupresaninmarko2@gmail.com>
2026-09-06 18:28:06 -0500

    updates pre launch

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: 'https://www.madhacks.io',
   redirects: {
-    "/apply": "https://airtable.com/appTh3C58aKBlgn0e/pagVgAsoZTgmN9Pmn/form",
+    "/apply": "https://airtable.com/appcsCoekwU6sZanl/pagVgAsoZTgmN9Pmn/form",
     "/walkin": "https://airtable.com/appTh3C58aKBlgn0e/pagDNAYeqCC6QqwSz/form",
     "/devpost": "https://madhacks-fall-2025.devpost.com",
     "/project": "https://airtable.com/appJa8ymShCTPpH9I/pag0C9Q3PGBC9qArU/form",
